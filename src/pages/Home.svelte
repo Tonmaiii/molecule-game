60e2634fb38370a43d51e2b3de1ff637d1615464
Tonmaiii <87060884+Tonmaiii@users.noreply.github.com>
2022-11-25 09:52:03 +0700

    Update Home.svelte

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,7 +2,7 @@
     <a href="/#/name">Name from formula</a> <br />
     <a href="/#/formula">Formula from name</a><br />
     <a href="/#/leaderboard">Leaderboard</a><br />
-    <h1>HPO3 2- is now biphosphate ion !!<h1/>
+    <h1>HPO3 2- is now biphosphite ion !!<h1/>
 </main>
 
 <style>
