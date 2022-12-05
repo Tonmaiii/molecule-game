4a5464f2305d2069e4983a445b6c2e28f5e23f95
Tonmaiii <87060884+Tonmaiii@users.noreply.github.com>
2022-12-05 15:11:15 +0700

    by Tonmaii

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,7 +2,7 @@
     <a href="/#/name">Name from formula</a> <br />
     <a href="/#/formula">Formula from name</a><br />
     <a href="/#/leaderboard">Leaderboard</a><br />
-    <h1>HPO3 2- is now biphosphite ion !!<h1/>
+    <h1><a href="https://github.com/tonmaiii">by Tonmaii</a><h1/>
 </main>
 
 <style>
