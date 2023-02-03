7624dc92176d55f9e789f5603f26705d5a65a1dd
Tonmaiii <pruek.rodsawat@gmail.com>
2023-02-03 09:16:51 +0700

    remade home page

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,17 +1,56 @@
 <main>
-    <a href="/#/name">Name from formula</a> <br />
-    <a href="/#/formula">Formula from name</a><br />
-    <a href="/#/leaderboard">Leaderboard</a>
+    <h1>Acid & Ion Game</h1>
+    <div>
+        <a href="/#/name">sulfuric acid</a>
+        <a href="/#/formula">H₂SO₄</a>
+        <a href="/#/leaderboard" class="leaderboard"
+            ><i class="fas fa-ranking-star" /></a
+        >
+    </div>
 </main>
 
 <style>
     main {
-        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        font-size: 4rem;
+    }
+
+    div {
+        display: flex;
+        width: 80%;
     }
 
     a {
+        display: flex;
         text-decoration: none;
-        color: black;
-        font-size: 4rem;
+        color: #c0c0c0;
+        aspect-ratio: 1/1;
+        flex: 1;
+        margin: 1rem;
+        background-color: #e8e8e8;
+        font-size: 5rem;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        border-radius: 1rem;
+    }
+
+    .leaderboard {
+        font-size: 8rem;
+    }
+
+    a:active:hover {
+        background-color: #c8c8c8;
+    }
+
+    @media (hover: hover) {
+        a:hover {
+            background-color: #d8d8d8;
+        }
     }
 </style>
